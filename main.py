994738f2c271a68e0de8b21356df8e357dc4f442
Beijar <patrikbeijar@gmail.com>
2015-03-08 12:49:57 +0100

    First prototype of SteamRate API

---
+++ main.py
@@ -0,0 +1,71 @@
+__author__ = 'patrikpirat & BaraMarcus'
+
+import json
+from urllib import urlopen, quote_plus as urlencode
+import unirest
+
+class Games(object):
+    def __init__(self, json):
+        self.__score = json['score']
+        self.__userScore = json['user_score']
+        self.__thumbnail = json['thumbnail']
+        self.__title = json['title']
+    pass
+
+
+def search_api(data):
+    pass
+
+
+def get_steam(userID):
+    with open('steam_key.txt', 'r') as f:
+        steam_key = f.read()
+
+    game_list = []
+    response = unirest.get("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key="
+                           + str(steam_key)
+                           + "&steamid=" + str(userID),
+                           headers={ "Accept": "application/json" },
+                           params={ "include_appinfo": "1", "include_played_free_games": "1" })
+
+    player_data = response.body
+    print player_data
+    player_games = player_data['response']['games']
+    for game in player_games:
+        game_list.append(game['name'])
+
+    get_metacritic(game_list)
+
+def get_metacritic(list):
+    #TO DO: FILTER THE DATA RESULT FOR BETTER EFFICENCY
+    game_data = []
+
+    with open('meta_key.txt') as keyfile:
+        meta_key = keyfile.read()
+
+    for game in list:
+
+        response = unirest.post("https://byroredux-metacritic.p.mashape.com/find/game",
+        headers={
+        "X-Mashape-Key": str(meta_key),
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Accept": "application/json"
+        },
+        params={
+        "platform": 3, #PC
+        "retry": 4,
+        "title": game
+         }
+        )
+
+        #game_data.append(response.body['response']) ['<<find right specc>>]
+
+        if response.body['result'] != False:
+            print response.body['result']['score']
+
+    #games = Games(game_data)
+    #search_api(games)
+
+
+get_steam(76561198007341040)
+
