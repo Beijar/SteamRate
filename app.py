bdc130d60aa4181544187e666ce9d07e1ace6598
Beijar <patrikbeijar@gmail.com>
2015-03-29 22:14:31 +0200

    First working beta version *celebrate*

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# coding: utf-8
+__author__ = 'PatrikPirat & Marcus'
 from flask import Flask, render_template, request, Response, jsonify
 import json
 from main import api_search, count_average
@@ -8,40 +8,51 @@ app = Flask(__name__)
 
 @app.route('/')
 def search_js():
+    #Main page
     return render_template("search.html")
 
 
 @app.route('/api/search')
 def search_api():
+    #API endpoint, takes one argument (Steam ID)
     query = request.args.get('q')
 
     if not query:
+        #if search submit is empty
         return jsonify({'error': 'Bad Request',
                         'code': 400,
                         'message': 'No user ID was provided'
                         }), 400
 
+    try:
+        user_game = api_search(query)
+        average_score = count_average(user_game)
 
-    user_game = api_search(query)
-    average_score = count_average(user_game)
+        return Response(json.dumps({"average":average_score, "result":user_game}, indent=4), status=200, mimetype='application/json')
+    except:
+        #if SteamRate API has an unexpected error
+        return jsonify({'error': 'Internal error',
+                        'code': 500,
+                        'message': 'Internal server error'
+                        }), 500
 
-    return Response(json.dumps({"average":average_score, "result":user_game}, indent=4), status=200, mimetype='application/json')
 
 @app.route('/docs')
 def docs():
+    #API documentation page
     return render_template("docs.html")
 
 
 @app.errorhandler(404)
 def page_not_found(error):
     if request.path.startswith("/api/"):
-        # API requests should get JSON
-        return jsonify({ 'error': 'Resource Not Found', 'code': 404, 'message': 'Requested endpoint does not exist' }), 404
+        # API requests should get JSON, props to Fredrik!
+        return jsonify({ 'error': 'Resource Not Found', 'code': 404, 'message': 'Page does not exist' }), 404
     else:
-        # Other requests should get HTML
+        # Other requests should get HTML.
         return render_template("error.html", code = 404, description = "Page not found"), 404
 
 
 if __name__ == '__main__':
-    app.debug = True
+    app.debug = True #remove for launch?
     app.run()
--- main.py
+++ main.py
@@ -1,24 +1,40 @@
-__author__ = 'patrikpirat & BaraMarcus'
+__author__ = 'PatrikPirat & Marcus'
+'''
+Date: 2015-03-29
+Description: Mashup API that takes a singel argument as input (a Steam ID).
+Extracts user owned games from the Steam API using the ID input then takes those games
+and use them as inputs to the unoficiall Metacritic API to extract score data to those games.
+Version: First beta version.
+'''
+
 
 import unirest
 
 
 def api_search(query):
-    sum_gameScore = []
+    '''
+    Top function that takes the user ID input and calls the Steam & Metacritic API functions.
+    When this function gets the return data it filters the result dictionaries in a loop before it returns
+    the completed result (sum_gameScore.
+    '''
+    sum_game_data = []
 
     user_games = get_steam(query)
     user_scores = get_metacritic(user_games)
 
     for scores in user_scores:
-        del scores['summary']
-        del scores['rating']
+        del scores['img_logo_url']
         del scores['publisher']
+        del scores['summary']
         del scores['platform']
-        sum_gameScore.append(scores)
+        del scores['appid']
+        del scores['img_icon_url']
+        sum_game_data.append(scores)
 
-    return sum_gameScore
+    return sum_game_data
 
 def count_average(game_score):
+    '''Function that computes the average user owned gamescore'''
     total_score = 0
     count = 0
 
@@ -35,10 +51,13 @@ def count_average(game_score):
     return average_result
 
 def get_steam(userID):
+    '''
+    Function that takes the user ID input and uses it in a GET call to Steam API
+    GetOwnedGames. The response is a json file of user owned games. Saves the response as a listed dict and returns it.
+    '''
     with open('steam_key.txt', 'r') as f:
         steam_key = f.read()
 
-    game_list = []
     response = unirest.get("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key="
                            + str(steam_key)
                            + "&steamid=" + str(userID),
@@ -48,20 +67,23 @@ def get_steam(userID):
     player_data = response.body
     player_games = player_data['response']['games']
 
-    for game in player_games:
-        game_list.append(game['name'])
-
-    return game_list
+    return player_games
 
 def get_metacritic(game_list):
+    '''
+    The function gets a listed dict of user owned games, response from Steam API call, as argument.
+    That argument is looped for calls to Metacritic API, each game titel is saved as a variable from
+    the dict and used as param to the Metacriti API call.
+    '''
     #TO DO: MULTITHREAD API CALLS
     game_data = []
-    gamestuff = game_list[0:10]
+    gamestuff = game_list[0:10] #Temp. restriction on API calls due to ineffiecency of loop calls.
 
     with open('meta_key.txt') as keyfile:
         meta_key = keyfile.read()
 
     for game in gamestuff:
+        title = game['name']
         response = unirest.post("https://byroredux-metacritic.p.mashape.com/find/game",
         headers={
         "X-Mashape-Key": str(meta_key),
@@ -71,12 +93,15 @@ def get_metacritic(game_list):
         params={
         "platform": 3, #PC
         "retry": 4,
-        "title": game
+        "title": title
          }
         )
 
         if response.body['result'] != False:
             game_score = response.body['result']
-            game_data.append(game_score)
+            combined_data = game.copy()
+            combined_data.update(game_score) #combining the game data from Steam API with the Metacritic API response.
+
+            game_data.append(combined_data)
 
     return game_data
